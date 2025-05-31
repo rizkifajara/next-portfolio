992d991cb610af80068945456d87fe575952bc95
rizkifajara <rizkifajar456@gmail.com>
2025-05-31 22:00:02 +0700

    fix: react syntax highlighter type

---
+++ src/types/react-icons.d.ts
@@ -0,0 +1,38 @@
+declare module 'react-icons/io5' {
+  import { ComponentType } from 'react';
+  
+  interface IconProps {
+    size?: string | number;
+    className?: string;
+    style?: React.CSSProperties;
+    [key: string]: any;
+  }
+  
+  export const IoClose: ComponentType<IconProps>;
+}
+
+declare module 'react-icons/fa' {
+  import { ComponentType } from 'react';
+  
+  interface IconProps {
+    size?: string | number;
+    className?: string;
+    style?: React.CSSProperties;
+    [key: string]: any;
+  }
+  
+  export const FaRobot: ComponentType<IconProps>;
+}
+
+declare module 'react-icons/io' {
+  import { ComponentType } from 'react';
+  
+  interface IconProps {
+    size?: string | number;
+    className?: string;
+    style?: React.CSSProperties;
+    [key: string]: any;
+  }
+  
+  export const IoMdSend: ComponentType<IconProps>;
+} 
